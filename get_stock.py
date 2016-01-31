44d371c8dc6466f402ef7d3a3e211956dccf7a1d
Mikel Bober-Irizar <mikel@mxbi.net>
2016-01-31 12:51:15 +0000

    Updated get_stock script
    Now allows for custom download arguments

--- get_stock.py
+++ get_stock.py
@@ -9,8 +9,13 @@ current_time = time.time()      # get current time
 stock_name = str(sys.argv[2])   # set stock to launch arg eg. IHG
 index_name = str(sys.argv[1])   # set index to launch arg eg. FTSE
 
+if len(sys.argv) == 3:  # if no argument given, use default
+    request_args = "sl1op"
+else:   # otherwise, use given args
+    request_args = str(sys.argv[3]) # set data download arguments to launch arg
+
 # Create URL
-url = "http://download.finance.yahoo.com/d/quotes.csv?s=%40%5E" + index_name + "," + stock_name + "&f=nsl1op"
+url = "http://download.finance.yahoo.com/d/quotes.csv?s=%40%5E" + index_name + "," + stock_name + "&f=" + request_args
 
 file_name = index_name + " " + stock_name + ".csv"
 u = urllib2.urlopen(url)
@@ -26,10 +31,8 @@ while True:
         break
 
     file_size_dl += len(buffer)
-    # Remove newline and stock name from buffer and inject current time
-    stock = buffer.replace('\n', '').split(",",1)
-    # Append current time and newline to buffer
-    towrite = stock[1] + "," + str(current_time) + "\n"
+    # inject unix time into buffer
+    towrite = buffer.replace('\n', '') + "," + str(current_time) + "\n"
     f.write(towrite) # Write to file
     
     # Status of download
